095892889e2e5493f8e9e11fe8be73563b3b3a3c
man-droid23 <alifilmannafian6b@gmail.com>
2023-06-13 22:55:20 +0700

    Fixing Issues with dowload model

--- inference.py
+++ inference.py
@@ -4,8 +4,7 @@ from PIL import Image
 from io import BytesIO
 import numpy as np
 import tensorflow as tf
-import requests
-# from tensorflow.keras.applications.efficientnet import preprocess_input, decode_predictions
+import urllib.request
 
 app = FastAPI()
 
@@ -21,11 +20,18 @@ app.add_middleware(
     allow_headers=headers,
 )
 
-url = "https://storage.googleapis.com/model-api-c23-pc662/model.h5"
-r = requests.get(url, allow_redirects=True)
+def download_model():
+    url = "https://storage.googleapis.com/model-api-c23-pc662/model.h5"
+    file_name = "model.h5"
+
+    urllib.request.urlretrieve(url, file_name)
+    print("File downloaded successfully.")
+
+download_model()
 
 model = tf.keras.models.load_model('model.h5')
 class_names = ['ayam bakar', 'ayam_goreng', 'ayam_pop', 'buncis', 'daging_rendang', 'dendeng_batokok', 'gulai_ikan', 'gulai_tambusu', 'gulai_tunjang', 'mie goreng', 'perkedel', 'sambal goreng kentang', 'sate', 'sayur asam', 'sayur bayam', 'sayur lodeh', 'sayur sop', 'tahu', 'telur_balado', 'telur_dadar', 'terong balado', 'tongkol balado', 'tumis kangkung', 'usus ayam bumbu kuning']
+calories_per_class_names = [300, 300, 300, 100, 300, 300, 300, 300, 300, 300, 100, 100, 300, 100, 100, 100, 100, 100, 100, 100, 300, 300, 100, 100]
 
 @app.get("/")
 async def root():
@@ -44,7 +50,7 @@ async def predict(file: UploadFile = None):
     prediction = model.predict(image)
     class_name_and_probability = []
     for i in range(len(prediction[0])):
-        class_name_and_probability.append({"class_name": class_names[i], "probability": float(prediction[0][i])})
+        class_name_and_probability.append({"class_name": class_names[i], "probability": float(prediction[0][i]), "calories": calories_per_class_names[i]})
     predicted_class = class_name_and_probability[np.argmax(prediction[0])]
     return {"predicted_class": predicted_class, "class_name_and_probability": class_name_and_probability}
 
